99ec80f84cf226753202127c3ed80b662f99648a
Alexandre <aleroperojunior@gmail.com>
2023-07-30 10:47:36 -0300

    primeiro commit

---
+++ src/main/java/ulife/com/br/TCCEngenhariaComputacao/TccEngenhariaComputacaoApplication.java
@@ -0,0 +1,13 @@
+package ulife.com.br.TCCEngenhariaComputacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccEngenhariaComputacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccEngenhariaComputacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ulife/com/br/TCCEngenhariaComputacao/TccEngenhariaComputacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package ulife.com.br.TCCEngenhariaComputacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccEngenhariaComputacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
